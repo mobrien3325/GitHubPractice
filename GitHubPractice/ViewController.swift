0d3bf3b8667ed03bd8b0eddede80348797665685
Mia <mdrago2107@stu.d214.org>
2022-04-04 08:53:54 -0700

    added code

--- GitHubPractice/ViewController.swift
+++ GitHubPractice/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var firstNumberTextField: UITextField!
+    @IBOutlet weak var secondNumberTextField: UITextField!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
-
+    @IBAction func whenAddButtonPressed(_sender:UIButton){
+        if let firstNumberString = firstNumberTextField.text, let secondNumberString = secondNumberTextField.text{
+        let firstNumber = Int(firstNumberString)!
+            let secondNumber = Int(secondNumberString)!
+        operationLabel.text = "+"
+            let answer = firstNumber + secondNumber
+            answerLabel.text = "\(answer)"
+        }
+    
+    
+    }
+    
 
 }
 
