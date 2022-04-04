fc3be85d849fc2e57cf035af40bf0e28fd195e76
Maggie OBrien <mobrien3325>
2022-04-04 10:30:47 -0500

    Initial Commit

---
+++ GitHubPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubPractice
+//
+//  Created by Maggie O'Brien on 4/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
